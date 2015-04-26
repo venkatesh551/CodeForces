d3a54f29666eb026c3280180943efeb4da1dcae5
Venkateswarlu Pallamala <p.venkatesh551@gmail.com>
2015-04-27 00:03:45 +0530

    Round #300

---
+++ Cutting_Banner.py
@@ -0,0 +1,27 @@
+def main():
+    s = raw_input()
+    t = "CODEFORCES"
+    m = len(s)
+    n = len(t)
+    ind = s.find(t)
+    if (ind == 0) or (ind + n) == m:
+        print 'YES'
+    elif ind == -1:
+        i, j = 0, 0
+        while (i < m) and (j < n) and (s[i] == t[j]):
+            i += 1
+            j += 1
+        k, l = m-1, n-1 
+        while k >= i and l >= j and s[k] == t[l]:
+            k -= 1
+            l -= 1
+        if l+1 == j:
+            print 'YES'
+        else:
+            print 'NO'
+    else:
+        print 'NO'
+
+if __name__ == '__main__':
+    while True:
+        main()
