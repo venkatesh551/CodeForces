48a86bfd548a41f41b2e47ba09bbf5e9a5648af3
Venkateswarlu Pallamala <p.venkatesh551@gmail.com>
2015-04-28 21:56:28 +0530

    Optimized

--- Cutting_Banner.py
+++ Cutting_Banner.py
@@ -3,22 +3,14 @@ def main():
     t = "CODEFORCES"
     m = len(s)
     n = len(t)
-    i, j = 0, 0
-    while (i < m) and (j < n) and (s[i] == t[j]):
-        i += 1
-        j += 1
-    k, l = m-1, n-1 
-    while k >= i and l >= j and s[k] == t[l]:
-        k -= 1
-        l -= 1
-    if l+1 == j:
-        print 'YES'
+    if len(s) < n:
+        print "NO"
     else:
-        ind = s.find(t)
-        if (ind == 0) or (ind != -1 and (ind + n) == m):
-            print 'YES'
-        else:
-            print 'NO'
+        for i in xrange(n+1):
+            if s[:n-i] + s[m-i:] == t:
+                print "YES"
+                return
+        print "NO"
 
 if __name__ == '__main__':
     main()
