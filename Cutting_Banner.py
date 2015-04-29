b1cb5c131800a854d40992fceee6a6964f621aed
Venkateswarlu Pallamala <p.venkatesh551@gmail.com>
2015-04-29 07:32:44 +0100

    Cutting edge.py chnaged

--- Cutting_Banner.py
+++ Cutting_Banner.py
@@ -12,16 +12,15 @@ def main():
             i += 1
             j += 1
         k, l = m-1, n-1 
-        while k >= i and l >= j and s[k] == t[l]:
+        while k > i and l > j and s[k] == t[l]:
             k -= 1
             l -= 1
-        if l+1 == j:
-            print 'YES'
+        if s[j] == s[k]:
+			print 'YES'
         else:
             print 'NO'
     else:
         print 'NO'
 
 if __name__ == '__main__':
-    while True:
-        main()
+    main()
